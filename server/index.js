04306883a93ded939c6be20dd5bf9f0abfaaac93
dkundel <dominik.kundel@gmail.com>
2019-04-03 12:14:56 -0700

    :bug: Change SSL redirect implementation

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import forceSsl from 'express-force-ssl';
 import { resolve } from 'path';
 import apiRouter from './api';
 import { setup } from './services/data';
+import { forceSsl } from './utils/ssl';
 import webhookRouter from './webhooks';
 
 const PORT = process.env.PORT || 4000;
@@ -11,7 +11,7 @@ const app = express();
 const isProd = process.env.NODE_ENV === 'production';
 
 if (isProd) {
-  // app.use(forceSsl);
+  app.use(forceSsl);
   app.use(express.static(resolve(__dirname, '../build')));
 }
 
@@ -19,7 +19,7 @@ app.use('/webhook', webhookRouter);
 app.use('/api', apiRouter);
 
 if (isProd) {
-  app.get('*', forceSsl, (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(resolve(__dirname, '../build/index.html'));
   });
 }
